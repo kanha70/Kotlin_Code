1cd87131790d4446f349fa12f92a1c33be8e7d97
Kanhaiya <kanhappti.kl@gmail.com>
2021-11-07 00:16:40 +0530

    Initial Commit

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+
+
+fun main(){
+    val name = "Kanhaiya"
+    val Age=25
+    val myFavoriteNumber = 15
+    println("My Name is: $name")
+    println("My Age is: $Age")
+    println("My Favourite Number is: $myFavoriteNumber")
+}
