1aae022f05f9f262ed3de3563c94bcf452ed3d12
Kanhaiya <kanhappti.kl@gmail.com>
2021-11-07 00:52:45 +0530

    Initial Commit

---
+++ src/Arithmatic_Operation.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+     var x=7
+     var y =8
+    // var result = x+y
+    // var result = 7+8
+    // println(result)
+    //println(7+8)
+    println("The value of result is: ${x+y}")
+    x += 10
+    println("The New value is: ${x+y}")
+}
---
+++ src/String.kt
@@ -0,0 +1,6 @@
+fun main(){
+    //val line  = "This line is written by Kanhiaya"
+    //println("My String is : ${line.uppercase().lowercase()}")
+    //   val name = "Kanhaiya"
+    //println(name.uppercase().reversed())
+}
