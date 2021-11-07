07fa366210a3b79e38cfeefda24532ff22a80801
Kanhaiya <kanhappti.kl@gmail.com>
2021-11-07 17:04:37 +0530

    Initial

--- src/Comparison.kt
+++ src/Comparison.kt
@@ -1,7 +1,24 @@
 fun main(){
-    val x = 3
-    val y = 5
-    println(x == y)
-    println(x < y)
+   // val x = 3
+   // val y = 5
+   //
+   // val a = 6
+   // val b = 10
+   // println(x != y && a !=b)
+
+    // && has higher precedence than || same like * > +
+
+    val simpleExpression = 3>4 || 4>3 && 4<=4
+
+    // println(simpleExpression)
+    // OR
+    // println((3>4 && 4>3) && 4>=4)
+
+    val bool = true
+    val x=9
+    val y=3
+    val z=9
+    val hardExpression = !(x !=z) && bool || z > (x+y) && (!bool || y < z)
+    println(hardExpression)
 
 }
---
+++ src/ConditionalOperator.kt
@@ -0,0 +1,16 @@
+
+ fun main(){
+
+    val x = 7
+    val y = 7
+    val z = if (x+y ==15) 3 else 4
+
+     if (x<y || y * y == 49){
+        println("Atleast one of the condition are true")
+    } else if(x>y){
+        println("x is greater than y")
+    }
+     else println("x and y both are equal")
+
+    println("Value of z is $z")
+}
