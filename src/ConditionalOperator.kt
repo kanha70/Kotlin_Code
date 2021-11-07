fadaa82ec4ad00d85a6d18d7046228611409263d
Kanhaiya <kanhappti.kl@gmail.com>
2021-11-07 17:08:28 +0530

    Initial

--- src/ConditionalOperator.kt
+++ src/ConditionalOperator.kt
@@ -5,11 +5,9 @@
     val y = 7
     val z = if (x+y ==15) 3 else 4
 
-     if (x<y || y * y == 49){
-        println("Atleast one of the condition are true")
-    } else if(x>y){
-        println("x is greater than y")
-    }
+     if (x<y || y * y == 49)  println("Atleast one of the condition are true")
+    else if(x>y)  println("x is greater than y")
+
      else println("x and y both are equal")
 
     println("Value of z is $z")
